d3992c18cb5fb8a0389ecd60e52bdec2ed21c7f3
mohand81 <anis.dhouieb@gmail.com>
2019-02-21 11:08:16 +0100

    create an autoloader for the classes

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,7 +1,15 @@
 <?php 
 
-// load libraries
+// load config file
 
-require_once 'libraries/Core.php';
-require_once 'libraries/Controller.php';
-require_once 'libraries/Database.php';
+
+require_once 'config/config.php' ;
+
+
+// autoload core libraries
+
+spl_autoload_register(function($classname){
+  
+  require_once 'libraries/'.$classname.'.php' ;
+
+});
